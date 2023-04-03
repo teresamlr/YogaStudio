72aada6ba330f7d1f313c35c41e1272b90c42b60
teresamlr Müller <mueller.teresa.a21@student.dhbw-karlsruhe.de>
2023-04-03 16:53:27 +0000

    n

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -59,48 +59,6 @@ class DatabaseFactory {
             ]);
         }
     }
-
-    /**
-     * Ersatz für den Konstruktor, damit aus dem Hauptprogramm heraus die
-     * Verbindungs-URL der MongoDB übergeben werden kann. Hier wird dann
-     * auch gleich die Verbindung hergestellt.
-     *
-     * @param {String} connectionUrl URL-String mit den Verbindungsdaten
-     */
-    async init(connectionUrl) {
-        // Datenbankverbindung herstellen
-        this.client = new MongoClient(connectionUrl);
-        await this.client.connect();
-        this.database = this.client.db("reviewlist");
-
-        await this._createDemoData();
-    }
-
-    /**
-     * Hilfsmethode zum Anlegen von Demodaten. Würde man so in einer
-     * Produktivanwendung natürlich nicht machen, aber so sehen wir
-     * wenigstens gleich ein paar Daten.
-     */
-    async _createDemoData() {
-        let registrations = this.database.collection("review");
-
-        if (await registrations.estimatedDocumentCount() === 0) {
-            registrations.insertMany([
-                {
-                    first_name: "Max",
-                    last_name: "Mustermann",
-                    course_name: "Yoga für Fortgeschrittene",
-                    text: ""
-                },
-                {
-                    first_name: "Erika",
-                    last_name: "Mustermann",
-                    course_name: "Weekend Flow",
-                    text: ""
-                }
-            ]);
-        }
-    }
 }
 
 export default new DatabaseFactory();
